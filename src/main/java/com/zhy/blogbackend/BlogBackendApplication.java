536d12d50e5c93aafb464712075f5c8d13d8de22
随缘而愈 <1908721295@qq.com>
2024-07-16 16:38:29 +0800

    feat: 简介

--- src/main/java/com/zhy/blogbackend/BlogBackendApplication.java
+++ src/main/java/com/zhy/blogbackend/BlogBackendApplication.java
@@ -4,6 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * 项目启动类
+ * @return
+ * @description TODO
+ */
 @SpringBootApplication
 @MapperScan("com.zhy.blogbackend.mapper")
 public class BlogBackendApplication {
